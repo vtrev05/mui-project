a844a87a7a04c7770ea036ff3550ab51b23875f4
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2023-03-28 09:26:29 +0200

    mui project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ResponsiveAppBar from './components/ResponsiveAppBar'
+
+function App() {
+
+  return (
+    <div className="App">
+     
+       <ResponsiveAppBar/>
+    </div>
+  )
+}
+
+export default App
